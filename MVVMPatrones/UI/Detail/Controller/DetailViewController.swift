684ad78f903afb23fee36195d1eacc19af4754b6
Natalia Camero Carreño <nataliacameroc@gmail.com>
2024-03-01 13:38:22 +0100

    🔥[DetailViewController] delete unnessary code

--- MVVMPatrones/UI/Detail/Controller/DetailViewController.swift
+++ MVVMPatrones/UI/Detail/Controller/DetailViewController.swift
@@ -22,18 +22,8 @@ class DetailViewController: UIViewController {
     
     var viewModel: DetailViewModelProtocol?//Comunicacion con el modelo
     
-    var dataCharacter: CharacterModel?
+//    var dataCharacter: CharacterModel?
 
-    init(dataCharacter: CharacterModel? = nil) {
-        self.dataCharacter = dataCharacter
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         if viewModel?.character != nil {
